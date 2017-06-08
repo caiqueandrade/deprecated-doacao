29d2faf94f54c0fc5b85016ce2cdc526646b8fdc
Caique Andrade <caiquembandrade@gmail.com>
2017-06-08 20:33:01 -0300

    Estrutura de Pastas e Bootstrap de Cadastro

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var watch = require('gulp-watch');
+
+var paths = [
+    "bower_components/angular/angular.min.js",
+    "bower_components/angular-ui-router/release/angular-ui-router.min.js",
+    "bower_components/angularfire/dist/angularfire.min.js",
+    "bower_components/angular-input-masks/angular-input-masks-standalone.min.js",
+    'js/**/*'
+];
+
+gulp.task('scripts', function () {
+    return gulp.src(paths)
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest('www/js/'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('js/**/*', ['scripts']);
+});
