37f96b8c39743275016d3babf21016feae990350
thiagogsilva <thiago.guimaraes.lino@gmail.com>
2017-06-15 00:25:04 -0300

    “TeladeLogin”

--- js/controllers/login.js
+++ js/controllers/login.js
@@ -1,5 +1,22 @@
 angular.module('doacao').controller('LoginController', LoginController);
 
 function LoginController($scope, $state){
+  var auth = $firebaseAuth();
+  $scope.dados = {};
+  $scope.login = login;
 
+  function login(){
+      // A única diferença desse controller para o de cadastro é a função
+      // usada na comunicação com o Firebase.
+      auth.$signInWithEmailAndPassword($scope.dados.email, $scope.dados.senha)
+      .then(loginSucesso).catch(loginErro);
+  }
+
+  function loginSucesso(){
+      $state.go('quadro');
+  }
+
+  function loginErro(erro){
+      console.log(erro)
+  }
 }
