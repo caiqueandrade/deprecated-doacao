a9302ea71e9b1cb9ff486e1c3256f47bef200b57
Caique Andrade <caiquembandrade@gmail.com>
2017-06-17 01:14:52 -0300

    Inicialização da função buscar (Google Maps API)

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -1,5 +1,37 @@
 angular.module('doacao').controller('HomeController', HomeController);
 
-function HomeController($scope, $state){
+function HomeController($scope, $http){
+    $scope.buscar = buscar;
+    $scope.buscarSucesso = buscarSucesso;
+    $scope.cep;
+    $scope.teste;
 
+    function buscar(){
+        var chaveGeocoding = 'AIzaSyAYRQ_bjjljKcoaLeSnYXcj0grJsX9xMXc';
+
+        var parametros = {
+            method: 'GET',
+            url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + $scope.cep + '&key=' + chaveGeocoding
+        }
+
+        $http(parametros).then(buscarSucesso, buscarErro);
+    }
+
+    function buscarSucesso(resposta){
+        if ($scope.cep.length == 8){
+            $scope.latitude = resposta.data.results[0].geometry.location.lat;
+            $scope.longitude = resposta.data.results[0].geometry.location.lng;
+            console.log('Sucesso');
+            console.log($scope.latitude);
+            console.log($scope.longitude);
+            console.log($scope.cep);
+        }
+        else {
+            console.log('CEP inválido.');
+        }
+    }
+
+    function buscarErro(){
+        console.log('Erro');
+    }
 }
